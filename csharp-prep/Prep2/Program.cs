fdc725ad85d0d3ff7b221d2be21acced829b8c14
Bryce-Jenkins-3 <btjszmath3@gmail.com>
2024-11-01 20:59:51 -0600

    git commit -m "Core requirement 1"

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+
+        string grade = Console.ReadLine();
+        int gradeInt = int.Parse(grade);
+
+        if (gradeInt >= 90)
+        {
+            Console.WriteLine("A");
+        }
+        else if (gradeInt >= 80)
+        {
+            Console.WriteLine("B");
+        }
+        else if (gradeInt >= 70)
+        {
+            Console.WriteLine("C");
+        }
+        else if (gradeInt >= 60)
+        {
+            Console.WriteLine("D");
+        }
+        else
+        {
+            Console.WriteLine("F");
+        }
     }
 }
