b44d725b6f4eee8ff2805a3620e83ce58f52fbfe
Bryce-Jenkins-3 <btjszmath3@gmail.com>
2024-11-02 13:59:12 -0600

    git commit -m "initial program"

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,39 @@ using System;
 
 class Program
 {
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.WriteLine("What is your name? ");
+        return Console.ReadLine();   
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.WriteLine("What is your favorite number? ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    static int SquareNumber(int number)
+    {
+        return number * number;
+    }
+
+    static void DisplayResult(int squareNumber, string userName)
+    {
+        Console.WriteLine($"The squared number is {squareNumber}.");
+        Console.WriteLine($"The user name is {userName}.");
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int number = PromptUserNumber();
+        int square = SquareNumber(number);
+        DisplayResult(square, userName);
     }
 }
