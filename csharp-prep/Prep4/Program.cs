5a47f52a3fd4de375df9e930b22fe7df9f46a3d9
Bryce-Jenkins-3 <btjszmath3@gmail.com>
2024-11-02 13:40:47 -0600

    git commit -m "core requirement 3"

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -9,6 +9,7 @@ class Program
         float sum = 0;
         float count = 0;
         float avg;
+        float max = 0;
 
         Console.WriteLine(" Enter a list of numbers, type 0 when finished.");
 
@@ -21,11 +22,16 @@ class Program
                 numbers.Add(newNumber);
                 sum += newNumber;
                 count ++;
+                if (newNumber > max)
+                {
+                    max = newNumber;
+                }
             } 
         } while (newNumber != 0);
 
         avg = sum / count;
         Console.WriteLine($"The sum is: {sum}");
         Console.WriteLine($"The average is {avg}");
+        Console.WriteLine($"The largest number is: {max}");
     }
 }
