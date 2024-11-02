abae35a0aefe2f863929bcb7596ce174be450df7
Bryce-Jenkins-3 <btjszmath3@gmail.com>
2024-11-02 13:29:12 -0600

    git commit -m "core requirement 1"

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int newNumber;
+        int sum = 0;
+
+        Console.WriteLine(" Enter a list of numbers, type 0 when finished.");
+
+        do
+        {
+            Console.Write("Enter Number: ");
+            newNumber = int.Parse(Console.ReadLine());
+            if (newNumber != 0)
+            {
+                numbers.Add(newNumber);
+                sum += newNumber;
+            } 
+        } while (newNumber != 0);
+
+        Console.WriteLine($"The sum is: {sum}");
     }
 }
