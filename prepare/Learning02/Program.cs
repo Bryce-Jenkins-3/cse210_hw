001382eedf315f808030fdf864feeda745e6f230
Bryce-Jenkins-3 <btjszmath3@gmail.com>
2024-11-02 22:16:06 -0600

    git Commit -m "steps 1-4"

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,35 @@
 using System;
 
+public class Job
+{
+    public string _company;
+    public string _jobTitle;
+    public int _startYear;
+    public int _endYear;
+
+    public void Display()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
+}
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "DELL";
+        job1._startYear = 2002;
+        job1._endYear = 2012;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Electrical Engineer";
+        job2._company = "Intel";
+        job2._startYear = 2012;
+        job2._endYear = 2020;
+
+        job1.Display();
+        job2.Display();
+        
     }
 }
