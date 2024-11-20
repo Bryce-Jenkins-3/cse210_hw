4c2c6ef9d03ad8336ae4f9dc464fc10d3c4bec14
Bryce-Jenkins-3 <btjszmath3@gmail.com>
2024-11-19 19:12:13 -0700

    finished prepare for inheritance

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment = new Assignment("Samuel Bennet", "Multiplication");
+        Console.WriteLine(assignment.GetSummary());
+        MathAssignment mathAssignment = new MathAssignment("Roberto Rodriquez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+        WritingAssignment writingAssignment = new WritingAssignment("Mary Waters", "Europearn History", "The Causes of World War II by Mary Waters");       
+        Console.WriteLine(writingAssignment.GetSummary());        
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
